517ebaf214958a3126b7da30bde65bcd529117f9
meshkovaalisa <meshkova_alisa51@mail.ru>
2022-02-04 19:12:52 +0300

    add search

--- 5.py
+++ 5.py
@@ -2,18 +2,20 @@ import sys
 import requests
 import geocoder
 
-ll, spn = geocoder.get_ll_spn(" ".join(sys.argv[1:]))
 
-map_params = {
-    "ll": ll,
-    "spn": spn,
-    "l": "map",
-    "pt": "{0},pm2dgl".format(ll)
-}
+def search(text):
+    ll, spn = geocoder.get_ll_spn(" ".join(sys.argv[1:]))
 
-map_api_server = "http://static-maps.yandex.ru/1.x/"
-response = requests.get(map_api_server, params=map_params)
+    map_params = {
+        "ll": ll,
+        "spn": spn,
+        "l": "map",
+        "pt": "{0},pm2dgl".format(ll)
+    }
 
-map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
+    map_api_server = "http://static-maps.yandex.ru/1.x/"
+    response = requests.get(map_api_server, params=map_params)
+
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
