1bfdccc85e33ad575e45c9061ee71c7056f1f5fb
meshkovaalisa <meshkova_alisa51@mail.ru>
2022-02-04 19:39:12 +0300

    separation into different functions

--- search_func.py
+++ search_func.py
@@ -2,19 +2,26 @@ import requests
 import geocoder
 
 
-def search(toponym):
-    ll, spn = geocoder.get_ll_spn(toponym)
-
+def map_parameters(ll, spn):
     map_params = {
         "ll": ll,
         "spn": spn,
         "l": "map",
         "pt": "{0},pm2dgl".format(ll)
     }
+    return map_params
 
+
+def make_map(map_params):
     map_api_server = "http://static-maps.yandex.ru/1.x/"
     response = requests.get(map_api_server, params=map_params)
 
     map_file = "map.png"
     with open(map_file, "wb") as file:
         file.write(response.content)
+
+
+def search(toponym):
+    ll, spn = geocoder.get_ll_spn(toponym)
+    map_params = map_parameters(ll, spn)
+    make_map(map_params)
